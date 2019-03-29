b200b3650c9e32270035e87dd5182c58d1689604
sa cux <fuoridallarete@users.noreply.github.com>
2019-03-29 17:36:10 +0100

    finished to generate the account number

--- Checking.java
+++ Checking.java
@@ -7,6 +7,7 @@ public class Checking extends Account{
   //constructor to initialize checking account properties
   public Checking(String name, String ssn, double initDeposit){
     super(name, ssn, initDeposit);
+    accountNumber = 2 + accountNumber;
     System.out.println("NEW CHECKING ACCOUNT");
   }
 
