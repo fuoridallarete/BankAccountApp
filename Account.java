abf8532dbfda82e883a1f2b04d3d2ca26088218c
sa cux <fuoridallarete@users.noreply.github.com>
2019-03-29 17:32:43 +0100

    Update Account.java
    generating account number following the instructions

--- Account.java
+++ Account.java
@@ -8,12 +8,28 @@ public abstract class Account implements IBaseRate{
   double balance;
   String accountNumber;
   double rate;
+  static int index = 10000;
   //constructor to set base properties and initialize the account
   public Account(String name, String ssn, double initDeposit){
     this.name = name;
     this.ssn = ssn;
     this.balance = initDeposit;
     System.out.println("Name: " + name + " SSN: " + ssn + " Balance: " + balance);
+    
+    //set account number
+    index++;
+    this.accountNumber = setAccountNumber();
+    System.out.println("Account number: " + this.accountNumber);
+  }
+  //generate account number
+  private String setAccountNumber(){
+    //get last 2-digits of the ssn
+    String lastTwoSsn = ssn.substring(ssn.length()-2);
+    //get unique 5-digit number
+    int uniqueId = index;
+    //get random 3-digit number
+    int randomNumber = (int) (Math.random() * Math.pow(10, 3));
+    return lastTwoSsn + uniqueId + randomNumber;
   }
 
   //list common methods
