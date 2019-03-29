03a38e4d03cb540831c65cca837f9baa05a552f2
sa cux <fuoridallarete@users.noreply.github.com>
2019-03-29 16:16:23 +0100

    Create Savings.java
    child class of Account

---
+++ Savings.java
@@ -0,0 +1,13 @@
+package bankAccountApp;
+
+public class Savings extends Account{
+  //list properties specific to the saving account
+
+  //contructor to initialize settings for the saving properties
+  public Savings(String name){
+    super(name);
+    System.out.println("NEW SAVINGS ACCOUNT");
+  }
+  //list any methods specific to savings account
+
+}
