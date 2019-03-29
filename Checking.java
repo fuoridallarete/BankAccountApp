07881dcefcbe9184666d5f39df202bff107d77fc
sa cux <fuoridallarete@users.noreply.github.com>
2019-03-29 16:16:00 +0100

    Create Checking.java
    children class of Account

---
+++ Checking.java
@@ -0,0 +1,14 @@
+package bankAccountApp;
+
+public class Checking extends Account{
+  //list properties specific to checking account
+
+  //constructor to initialize checking account properties
+  public Checking(String name){
+    super(name);
+    System.out.println("NEW CHECKING ACCOUNT");
+  }
+
+  //list any methods specific to the checking account
+
+}
