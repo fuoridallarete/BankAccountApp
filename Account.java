1f4809ffacd1861f596c686363c41f7518f70501
sa cux <fuoridallarete@users.noreply.github.com>
2019-03-29 16:30:31 +0100

    Update Account.java
    list common properties for checking and savings accounts

--- Account.java
+++ Account.java
@@ -3,7 +3,11 @@ package bankAccountApp;
 //it's abstract so we won't be able to create account objects (we will create them from its children checking and savings class)
 public abstract class Account implements IBaseRate{
   //list common properties for checking and savings accounts
-
+  String name;
+  String ssn;
+  double balance;
+  String accountNumber;
+  double rate;
   //constructor to set base properties and initialize the account
   public Account(String name){
     System.out.println("Name: " + name);
